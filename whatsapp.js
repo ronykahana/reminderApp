c0c1879828b7c8d97b8d4cc8969e75b04c9728f8
ronykahana <35880798+ronykahana@users.noreply.github.com>
2026-01-03 01:06:37 -0500

    Refactor WhatsApp message sending functions
    Removed the sendTemplate function and refactored the sendTextMessage function to include logging to Airtable.

--- whatsapp.js
+++ whatsapp.js
@@ -1,25 +1,4 @@
-export async function sendTemplate(to, templateName) {
-  const url = `https://graph.facebook.com/v23.0/${process.env.PHONE_ID}/messages`;
-
-  const res = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Authorization": `Bearer ${process.env.ACCESS_TOKEN}`,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      messaging_product: "whatsapp",
-      to,
-      type: "template",
-      template: {
-        name: templateName,
-        language: { code: "en_US" }
-      }
-    })
-  });
-
-  return res.json();
-}
+import { logToAirtable } from "./airtable.js";
 
 export async function sendTextMessage(to, text) {
   const url = `https://graph.facebook.com/v23.0/${process.env.PHONE_ID}/messages`;
@@ -41,33 +20,7 @@ export async function sendTextMessage(to, text) {
   });
 
   const data = await response.json();
-  console.log("Sent message:", data);
-}
 
-import { logToAirtable } from "./airtable.js";
-
-// export async function sendTextMessage(to, text) {
-//   const url = `https://graph.facebook.com/v23.0/${process.env.PHONE_NUMBER_ID}/messages`;
-
-//   const body = {
-//     messaging_product: "whatsapp",
-//     to,
-//     type: "text",
-//     text: { body: text }
-//   };
-
-//   const response = await fetch(url, {
-//     method: "POST",
-//     headers: {
-//       Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(body)
-//   });
-
-//   const data = await response.json();
-
-  // ✅ LOG SENT MESSAGE
   await logToAirtable({
     direction: "sent",
     phone: to,
@@ -78,6 +31,5 @@ import { logToAirtable } from "./airtable.js";
     raw: data
   });
 
-  return data;
+  return data; // ✅ now INSIDE function
 }
-
