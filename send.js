aa63f19adf57782d5b5491262c00c28125221c33
ronykahana <35880798+ronykahana@users.noreply.github.com>
2026-01-04 12:45:10 -0500

    Clean up send.js by removing unused code
    Removed commented-out code for sending messages via WhatsApp API.

--- send.js
+++ send.js
@@ -1,25 +1,3 @@
-// const response = await fetch(
-//   `https://graph.facebook.com/v23.0/${PHONE_NUMBER_ID}/messages`,
-//   {
-//     method: "POST",
-//     headers: {
-//       "Authorization": `Bearer ${ACCESS_TOKEN}`,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//       messaging_product: "whatsapp",
-//       to: toPhone,
-//       type: "template",
-//       template: {
-//         name: "your_template",
-//         language: { code: "en_US" }
-//       }
-//     })
-//   }
-// );
-
-// const data = await response.json();
-// const messageId = data.messages?.[0]?.id;
 import express from "express";
 import { sendTextMessage } from "./whatsapp.js";
 import { logToAirtable } from "./airtable.js";
