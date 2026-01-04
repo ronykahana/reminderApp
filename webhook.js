71966e4d1c049963bb9690c1423f31228a51057f
ronykahana <35880798+ronykahana@users.noreply.github.com>
2026-01-04 13:13:59 -0500

    Remove unused import from webhook.js
    Removed unused import of logToAirtableSimple.

--- webhook.js
+++ webhook.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { sendTextMessage } from "./whatsapp.js";
 import { logToAirtable } from "./airtable.js";
-import { logToAirtableSimple } from "./airtable.js";
 
 const router = express.Router();
 
