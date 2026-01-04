0db07f5589885442f01ec5940ebf78df22df832d
ronykahana <35880798+ronykahana@users.noreply.github.com>
2026-01-04 12:47:10 -0500

    Add function to send template messages via WhatsApp

--- whatsapp.js
+++ whatsapp.js
@@ -33,3 +33,25 @@ export async function sendTextMessage(to, text) {
 
   return data; // ✅ now INSIDE function
 }
+
+export async function sendTemplateMessage(to, template,lan) {
+const response = await fetch(
+  `https://graph.facebook.com/v23.0/${PHONE_NUMBER_ID}/messages`,
+  {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${ACCESS_TOKEN}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      messaging_product: "whatsapp",
+      to: to,
+      type: "template",
+      template: {
+        name: template,
+        language: { code: lan }
+      }
+    })
+  }
+);
+}
